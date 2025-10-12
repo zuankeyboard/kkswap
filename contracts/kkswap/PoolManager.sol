cb3c0756186e5986a546900d626765a0d6e1c7f0
lumos <522438869@qq.com>
2025-10-12 20:37:42 +0800

    feat(PoolManager): 实现获取所有池子信息和创建池子的功能
    添加 getAllPools 方法获取所有池子的详细信息
    完善 createAndInitializePoolIfNecessary 方法实现池子创建和初始化
    添加测试用例验证功能正确性

--- contracts/kkswap/PoolManager.sol
+++ contracts/kkswap/PoolManager.sol
@@ -2,8 +2,9 @@
 pragma solidity ^0.8.24;
 pragma abicoder v2;
 
-import "./interfaces/IPoolManager.sol";
 import "./Factory.sol";
+import "./interfaces/IPool.sol";
+import "./interfaces/IPoolManager.sol";
 
 contract PoolManager is Factory, IPoolManager {
     Pair[] public pairs;
@@ -24,14 +25,67 @@ contract PoolManager is Factory, IPoolManager {
         // 然后获取每个 pool 的信息
         // 然后返回
         // 先 mock
-        poolsInfo = new PoolInfo[](pairs.length);
+        uint32 length = 0;
+        // 先算一下大小，从 pools 获取
+        for (uint32 i = 0; i < pairs.length; i++) {
+            length += uint32(pools[pairs[i].token0][pairs[i].token1].length);
+        }
+
+        // 再填充数据
+        poolsInfo = new PoolInfo[](length);
+        uint256 index
+        for (uint32 i = 0; i < pairs.length; i++) {
+            address[] memory addresses = pools[pairs[i].token0][pairs[i].token1];
+            for (uint32 j = 0; j < addresses.length; j++) {
+                IPool pool = IPool(addresses[j]);
+                poolsInfo[index] = PoolInfo({
+                    token0: pool.token0(),
+                    token1: pool.token1(),
+                    index: j,
+                    fee: pool.fee(),
+                    feeProtocol: 0,
+                    tickLower: pool.tickLower(),
+                    tickUpper: pool.tickUpper(),
+                    tick: pool.tick(),
+                    sqrtPriceX96: pool.sqrtPriceX96()
+                });
+                index++;
+            }
+        }
         return poolsInfo;
     }
 
     function createAndInitializePoolIfNecessary(
         CreateAndInitializeParams calldata params
-    ) external payable override returns (address pool) {
+    ) external payable override returns (address poolAddress) {
         // 如果没有对应的 Pool 就创建一个 Pool
         // 创建成功后记录到 pairs 中
+        require(params.token0 < params.token1);
+
+        poolAddress = this.createPool(
+            params.token0,
+            params.token1,
+            params.tickLower,
+            params.tickUpper,
+            params.fee
+        );
+
+        // 获取池子合约
+        IPool pool = IPool(poolAddress);
+
+        // 获取同一交易对的数量
+        uint256 index = pools[pool.token0()][pool.token1()].length;
+
+        // 新创建的池子，没有初始化价格，需要初始化价格
+        if (pool.sqrtPriceX96() == 0) {
+            pool.initialize(params.sqrtPriceX96);
+
+            if (index == 1) {
+                // 如果是第一次添加该交易对，需要记录下来
+                pairs.push(
+                    Pair({token0: pool.token0(), token1: pool.token1()})
+                );
+            }
+        }
     }
 }
