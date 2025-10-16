f3409f991667a7c4998c906a1b58447139b1ee82
lumos <522438869@qq.com>
2025-10-16 17:33:42 +0800

    feat(swap): 实现swap功能并添加测试合约
    添加TestSwap测试合约用于验证swap功能
    在Pool.sol中实现swap函数逻辑，包括价格计算、手续费处理和资金转移
    更新README.md添加Uniswap V3流动性模型公式推导说明

--- contracts/kkswap/Pool.sol
+++ contracts/kkswap/Pool.sol
@@ -279,11 +279,165 @@ contract Pool is IPool {
         emit Burn(msg.sender, amount, amount0, amount1);
     }
 
+    // 交易中需要临时存储的变量
+    struct SwapState {
+        // the amount remaining to be swapped in/out of the input/output asset
+        int256 amountSpecifiedRemaining;
+        // the amount already swapped out/in of the output/input asset
+        int256 amountCalculated;
+        // current sqrt(price)
+        uint160 sqrtPriceX96;
+        // the global fee growth of the input token
+        uint256 feeGrowthGlobalX128;
+        // 该交易中用户转入的 token0 的数量
+        uint256 amountIn;
+        // 该交易中用户转出的 token1 的数量
+        uint256 amountOut;
+        // 该交易中的手续费，如果 zeroForOne 是 ture，则是用户转入 token0，单位是 token0 的数量，反正是 token1 的数量
+        uint256 feeAmount;
+    }
+
     function swap(
         address recipient,
         bool zeroForOne,
         int256 amountSpecified,
         uint160 sqrtPriceLimitX96,
         bytes calldata data
-    ) external override returns (int256 amount0, int256 amount1) {}
+    ) external override returns (int256 amount0, int256 amount1) {
+        require(amountSpecified != 0, "AS");
+
+        // zeroForOne: 如果从 token0 交换 token1 则为 true，从 token1 交换 token0 则为 false
+        // 判断当前价格是否满足交易的条件
+        require(
+            zeroForOne
+                ? sqrtPriceLimitX96 < sqrtPriceX96 &&
+                    sqrtPriceLimitX96 > TickMath.MIN_SQRT_PRICE
+                : sqrtPriceLimitX96 > sqrtPriceX96 &&
+                    sqrtPriceLimitX96 < TickMath.MAX_SQRT_PRICE,
+            "SPL"
+        );
+
+        // amountSpecified 大于 0 代表用户指定了 token0 的数量，小于 0 代表用户指定了 token1 的数量
+        bool exactInput = amountSpecified > 0;
+
+        SwapState memory state = SwapState({
+            amountSpecifiedRemaining: amountSpecified,
+            amountCalculated: 0,
+            sqrtPriceX96: sqrtPriceX96,
+            feeGrowthGlobalX128: zeroForOne
+                ? feeGrowthGlobal0X128
+                : feeGrowthGlobal1X128,
+            amountIn: 0,
+            amountOut: 0,
+            feeAmount: 0
+        });
+
+        // 计算交易的上下限，基于 tick 计算价格
+        uint160 sqrtPriceX96Lower = TickMath.getSqrtPriceAtTick(tickLower);
+        uint160 sqrtPriceX96Upper = TickMath.getSqrtPriceAtTick(tickUpper);
+        // 计算用户交易价格的限制，如果是 zeroForOne 是 true，说明用户会换入 token0，会压低 token0 的价格（也就是池子的价格），所以要限制最低价格不能超过 sqrtPriceX96Lower
+        uint160 sqrtPriceX96PoolLimit = zeroForOne
+            ? sqrtPriceX96Lower
+            : sqrtPriceX96Upper;
+
+        // 计算交易的具体数值
+        (
+            state.sqrtPriceX96,
+            state.amountIn,
+            state.amountOut,
+            state.feeAmount
+        ) = SwapMath.computeSwapStep(
+            sqrtPriceX96,
+            (
+                zeroForOne
+                    ? sqrtPriceX96PoolLimit < sqrtPriceLimitX96
+                    : sqrtPriceX96PoolLimit > sqrtPriceLimitX96
+            )
+                ? sqrtPriceLimitX96
+                : sqrtPriceX96PoolLimit,
+            liquidity,
+            amountSpecified,
+            fee
+        );
+
+        // 更新新的价格
+        sqrtPriceX96 = state.sqrtPriceX96;
+        tick = TickMath.getTickAtSqrtPrice(state.sqrtPriceX96);
+
+        // 计算手续费
+        state.feeGrowthGlobalX128 += FullMath.mulDiv(
+            state.feeAmount,
+            FixedPoint128.Q128,
+            liquidity
+        );
+
+        // 更新手续费相关信息
+        if (zeroForOne) {
+            feeGrowthGlobal0X128 = state.feeGrowthGlobalX128;
+        } else {
+            feeGrowthGlobal1X128 = state.feeGrowthGlobalX128;
+        }
+
+        // 计算交易后用户手里的 token0 和 token1 的数量
+        if (exactInput) {
+            state.amountSpecifiedRemaining -= (state.amountIn + state.feeAmount)
+                .toInt256();
+            state.amountCalculated = state.amountCalculated.sub(
+                state.amountOut.toInt256()
+            );
+        } else {
+            state.amountSpecifiedRemaining += state.amountOut.toInt256();
+            state.amountCalculated = state.amountCalculated.add(
+                (state.amountIn + state.feeAmount).toInt256()
+            );
+        }
+
+        (amount0, amount1) = zeroForOne == exactInput
+            ? (
+                amountSpecified - state.amountSpecifiedRemaining,
+                state.amountCalculated
+            )
+            : (
+                state.amountCalculated,
+                amountSpecified - state.amountSpecifiedRemaining
+            );
+
+        if (zeroForOne) {
+            // callback 中需要给 Pool 转入 token
+            uint256 balance0Before = balance0();
+            ISwapCallback(msg.sender).swapCallback(amount0, amount1, data);
+            require(balance0Before.add(uint256(amount0)) <= balance0(), "IIA");
+
+            // 转 Token 给用户
+            if (amount1 < 0)
+                TransferHelper.safeTransfer(
+                    token1,
+                    recipient,
+                    uint256(-amount1)
+                );
+        } else {
+            // callback 中需要给 Pool 转入 token
+            uint256 balance1Before = balance1();
+            ISwapCallback(msg.sender).swapCallback(amount0, amount1, data);
+            require(balance1Before.add(uint256(amount1)) <= balance1(), "IIA");
+
+            // 转 Token 给用户
+            if (amount0 < 0)
+                TransferHelper.safeTransfer(
+                    token0,
+                    recipient,
+                    uint256(-amount0)
+                );
+        }
+
+        emit Swap(
+            msg.sender,
+            recipient,
+            amount0,
+            amount1,
+            sqrtPriceX96,
+            liquidity,
+            tick
+        );
+    }
 }
